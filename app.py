3029124cfff7f231408946a12dcf3b5fe52f5ad3
Hub-Varith <haemavitch@gmail.com>
2021-10-17 18:57:50 +0700

    updated app name

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask.json import jsonify
+from model import Model
+from flask import request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<p>Main page</p>"
+
+@app.route("/predict", methods = ['POST'])
+def predict_emo():
+    if request.method == 'POST':
+        x = request.json['textdata']
+        prd = model.predict(x)
+        return jsonify(result = str(prd))
+
+if __name__ == "__main__":
+    model = Model()
+    app.run(debug=False)
+
+
+
